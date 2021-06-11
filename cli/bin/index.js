898003e8b217d9579fb4ed4768a2c7b82d4d792f
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-06-11 17:56:47 +0200

    chore: cli works with workspace

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+import * as Nodelint from "@nodelint/core";
 
-console.log('Hello from cli')
+async function main() {
+    const policies = await Nodelint.load();
+
+    await Nodelint.run(void 0, policies);
+}
+main().catch(console.error);
